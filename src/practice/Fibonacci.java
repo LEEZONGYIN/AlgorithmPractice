4289a1a6cc71bb2e79974b69445c02392c5fd63c
LEEZONGYIN <673368374@qq.com>
2020-08-28 16:09:11 +0800

    2020/8/28

--- src/practice/Fibonacci.java
+++ src/practice/Fibonacci.java
@@ -17,6 +17,6 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
-        System.out.println(fibonacci.Fibonacci(3));
+        System.out.println(fibonacci.Fibonacci(6));
     }
 }
