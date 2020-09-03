9c2a718c5e5ac4966edd26d74f72199709d77b91
LEEZONGYIN <673368374@qq.com>
2020-09-03 19:58:16 +0800

    2020/9/3

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,36 +6,43 @@ import org.omg.PortableInterceptor.INACTIVE;
 import java.util.*;
 
 public class Test {
-    List<List<Integer>> res = new ArrayList<>();
-    public int get7Nums(int[] nums){
-        ArrayList<Integer> list = new ArrayList<>();
-        backtrace(nums,list);
-        System.out.println(res);
-        return res.size();
-    }
-    public void backtrace(int[] nums,List<Integer> path){
-        if(path.size() == nums.length){
-            int sum = 0;
-            for(int i:path){
-                sum = sum*10+nums[i];
-            }
-            if(sum %7==0){
-                res.add(new ArrayList<>(path));
-            }
-            return;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(path.contains(i)) continue;
-            path.add(i);
-            backtrace(nums,path);
-            path.remove(path.size()-1);
-        }
-    }
+    //使用StringBuilder进行转换
+
     public static void main(String[] args) {
 
-        Test t = new Test();
-        int[] nums = {1,1,2};
-        System.out.println(t.get7Nums(nums));
+       int x = 7;
+       int y = 6;
+        System.out.println(x++*--y);
+    }
+
+    public String[] getPokerOrder(String[] cards){
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('k',1);map.put('s',2);map.put('h',3);map.put('p',4);map.put('q',5);
+        HashMap<Integer,Character> map1 = new HashMap<>();
+        map1.put(1,'k'); map1.put(2,'s');map1.put(3,'h');map1.put(4,'p');map1.put(5,'q');
+        int[][] arr = new int[cards.length][2];
+        for(int i=0;i<cards.length;i++){
+            char ch = cards[i].charAt(0);
+            arr[i][0] = map.get(ch);
+            arr[i][1] = Integer.parseInt(cards[i].substring(1));
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0]==o2[0]) return o1[1]-o2[1];
+                return o1[0]-o2[0];
+            }
+        });
+
+        String[] res = new String[cards.length];
+        for(int i=0;i<res.length;i++){
+            StringBuilder sb = new StringBuilder();
+            sb.append(map.get(arr[i][0]));
+            sb.append(arr[i][1]);
+            res[i] = sb.toString();
+        }
+
+        return res;
 
     }
 }
